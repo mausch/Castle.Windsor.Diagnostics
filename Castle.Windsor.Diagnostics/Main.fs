5ff8f26f0ac4b67b3936747b53ee44e8475491d4
mausch <mauricioscheffer@gmail.com>
2010-09-11 19:21:13 -0300

    cmdline arg parsing stub, dependencies

--- Castle.Windsor.Diagnostics/Main.fs
+++ Castle.Windsor.Diagnostics/Main.fs
@@ -1,5 +1,27 @@
 ﻿module Main
 
+open Castle.MicroKernel
+open Castle.Windsor
+
+let loadKernel (xmlFile: string) =
+    let container = new WindsorContainer(xmlFile)
+    container.Kernel
+
+
+
 [<EntryPoint>]
-let main args = 0
+let main args = 
+    let compile s = printfn "Compiling %s..." s
+    let outputName = ref "a.out"
+    let verbose = ref false
+    let warningLevel = ref 0
+    let kk = ArgInfo("", ArgType.String ignore, "")
+    let setref d s = d := s
+    let specs = [
+                "-o", ArgType.String (setref outputName), "Name of the output"
+                "-v", ArgType.Set verbose, "Display additional information"
+                "--warn", ArgType.Int (setref warningLevel), "Set warning level"
+                "--", ArgType.Rest compile, "Stop parsing command line"
+                ] |> List.map (fun (sh, ty, desc) -> ArgInfo(sh, ty, desc))
+    0
 
